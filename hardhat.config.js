d22ecc41a6963d564034cf0c845479c73615ecce
Sehrish Khalid <sehrishkhalid533@gmail.com>
2022-10-22 01:27:14 +0500

    second commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,8 @@ const { task } = require("hardhat/config")
 require("@nomicfoundation/hardhat-toolbox")
 require("@nomiclabs/hardhat-ethers")
 require("@nomiclabs/hardhat-etherscan")
+require("hardhat-gas-reporter")
+require("hardhat-deploy")
 require("dotenv").config()
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
@@ -19,9 +21,26 @@ module.exports = {
             accounts: [PRIVATE_KEY],
             chainId: 5,
         },
+        localHost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
     },
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
     },
     solidity: "0.8.17",
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        //currency: "USD",
+        //coinmarketcap: COINMARKETCAP_API_KEY, // for usd we need coin market cap api key
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+    },
 }
